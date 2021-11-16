ecb2d724c87a8634316ec9714a9d955b7bc0bae6
Johann Mendoza <johannmendoza370@gmail.com>
2021-11-16 13:07:47 -0600

    created user model

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from typing import Text
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=Text("NOW()"))
